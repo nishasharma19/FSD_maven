94c6e311076a5a914b49237bb1da9c468e513db7
b8ibmjava32@iiht.tech <b8ibmjava32@iiht.tech>
2019-11-06 09:38:39 +0530

    06-11-2019'

---
+++ spring-rest-application-h2/src/main/java/com/mycompany/spring_rest_application/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.mycompany.spring_rest_application.dao;
+
+import java.util.List;
+
+import com.mycompany.spring_rest_application.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public Customer getCustomer(int theId);
+
+	public Customer save(Customer customer);
+
+	public void deleteCustomer(int theId);
+
+}
--- spring-rest-application/src/main/java/com/mycompany/spring_rest_application/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.spring_rest_application.service;
-
-public interface LoginService {
-	public boolean validateUsername(String username);
-	public boolean validatePassword(String password);
-
-}
